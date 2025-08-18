0d756f532645c64925f6cd8d4477379d6b49bbf5
AramiDA <adeamoriza@excelsis.com.py>
2025-08-18 19:50:11 +0000

    Estado inicial

---
+++ test.py
@@ -0,0 +1,13 @@
+# Archivo: prueba.py
+# Proyecto: Mi-Proyecto
+# Autor: Arami
+# Descripción: Script simple para probar Git y GitHub
+
+def saludar(nombre):
+    """Función que devuelve un saludo personalizado."""
+    return f"¡Hola, {nombre}! Bienvenida a tu primer proyecto con GitHub 🚀"
+
+# Punto de entrada principal
+if __name__ == "__main__":
+    # Nombre
+    print(saludar("Arami"))
